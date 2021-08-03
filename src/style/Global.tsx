5d83b4582f047d417446201b3ade263acb33f12f
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-02 20:58:40 -0700

    done

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,6 +1,7 @@
-import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
-import { SaltTheme } from '@saltswap/uikit/dist/theme'
+
+import { SaltTheme } from '@saltswap/uikit'
+import { createGlobalStyle } from 'styled-components'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
